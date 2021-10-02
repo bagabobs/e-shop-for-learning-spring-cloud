fb5a7bf96995e445397e3ad07440b31dc1ca37c9
baga gusdiana <bagaman83@gmail.com>
2021-10-02 08:53:18 +0700

    first init

---
+++ recomendation-service/src/test/java/com/learn/springcloud/recomendationservice/RecomendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.recomendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
